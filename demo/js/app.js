c97dc7d3fea59b58b5ae5b248d15cb80d529a846
Prashant Agrawal <prashant@enthought.com>
2014-07-05 02:04:24 +0530

    Basic UI code for examples view.

--- demo/js/app.js
+++ demo/js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('DemoApp', ['jigna']);
 
 app.controller('DemoController', function($scope){
     $scope.active_view = 0;
+    $scope.active_mode = 'examples';
 
     $scope.update_active = function(index){
         $scope.active_view = index;
