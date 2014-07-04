1791b30ddabb722443ff0015f7a5bf085b7318f1
Prashant Agrawal <prashant@enthought.com>
2014-07-04 17:49:09 +0530

    WIP: Better structure for the examples_server.
    WIP because I'm getting an angular error.

--- examples_server.py
+++ examples_server.py
@@ -1,10 +1,36 @@
 from subprocess import Popen
 from os.path import join, expanduser
-from traits.api import HasTraits, Str, List
+from traits.api import HasTraits, Str, List, Property
 from jigna.api import Template, WebApp
 
 
 #### Domain model ####
+
+class Example(HasTraits):
+    """
+    A class representing a jigna example. It contains the human readable name of
+    the example, the filename of the example and the codestring that tells the
+    domain model and the template used in that model.
+    """
+
+    #: Root of the examples directory
+    root = Str
+
+    #: Filename of the example
+    filename = Str
+
+    #: Human readable name of the example
+    name = Property(Str, depends_on='filename')
+    def _get_name(self):
+        return self.filename.replace("_", " ").capitalize()
+
+    def run(self):
+        """
+        Run the example
+        """
+        cmd = ['python', join(self.root, self.filename)]
+        Popen(cmd, cwd=self.root)
+        print "returning now"
 
 class ExamplesServer(HasTraits):
     """
@@ -13,32 +39,40 @@ class ExamplesServer(HasTraits):
     """
 
     root = Str
-    def _root_default(self):
-        return expanduser('~/work/jigna/examples/')
+
+    examples = Property(List(Example), depends_on='selected_examples')
+    def _get_examples(self):
+        examples = []
+        for example_name in self.selected_examples:
+            examples.append(Example(root=self.root, filename=example_name+".py"))
+
+        return examples
 
     selected_examples = List(Str)
     def _selected_examples_default(self):
-        return ['simple_view', 'model_updates', 'method_call', 'method_call_slow',
-            'list_of_instances', 'instance_trait', 'event_trait', 'success_error_callbacks',
-            'simple_view_web', 'embedding_chaco', 'embedding_mayavi',
-            'embedding_in_qt']
-
-    def run_example(self, example_name):
-        example_file = join(self.root, example_name + '.py')
-        cmd = ['python', example_file]
-        Popen(cmd, cwd=self.root)
-
+        return [
+            'simple_view',
+            'model_updates',
+            'method_call',
+            'method_call_slow',
+            'list_of_instances',
+            'instance_trait',
+            'event_trait',
+            'success_error_callbacks',
+            'simple_view_web',
+            'embedding_chaco',
+            'embedding_in_qt'
+        ]
 
 #### UI layer ####
 
 template = Template(html_file='presentation/index.html', base_url='presentation')
 
 if __name__ == "__main__":
-    examples_server = ExamplesServer()
+    examples_server = ExamplesServer(root=expanduser('~/work/jigna/examples'))
     app = WebApp(
         template=template,
         context={'examples_server': examples_server},
-        port=8000
     )
 
     app.start()
