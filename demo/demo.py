1593db61ed0acbe1f271d15dac03debd44192e45
Prashant Agrawal <prashant@enthought.com>
2014-07-05 02:53:29 +0530

    Add highlighting support and other minor improvements.

--- demo/demo.py
+++ demo/demo.py
@@ -1,7 +1,7 @@
 from jigna.api import Template, WebApp
 
 
-template = Template(html_file='demo.html')
+template = Template(html_file='demo.html', recommended_size=(1400, 800))
 
 app = WebApp(template=template, context={}, port=8000)
 app.start()
