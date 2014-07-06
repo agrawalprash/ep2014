f3107477e4f2e0bd9561e3fa1eef8943019a3857
Prashant Agrawal <prashant@enthought.com>
2014-07-06 12:45:24 +0530

    Add the ability to change view mode

--- demo/js/app.js
+++ demo/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('DemoApp', ['jigna']);
 
 app.controller('DemoController', function($scope){
     $scope.active_example = 0;
-    $scope.active_mode = 'examples';
+    $scope.active_mode = 'presentation';
 
     $scope.update_active_example = function(index){
         $scope.active_example = index;
