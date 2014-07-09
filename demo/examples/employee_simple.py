6230072b379fc4be079041e5fa2c51f6f134f2b4
Prashant Agrawal <prashant@enthought.com>
2014-07-09 12:07:01 +0530

    Add an example for the employee view.

---
+++ demo/examples/employee_simple.py
@@ -0,0 +1,37 @@
+#### Imports ####
+from traits.api import HasTraits, Str, Int
+from jigna.api import Template, QtApp
+
+#### Domain model ####
+
+class Employee(HasTraits):
+    name = Str
+    salary = Int
+
+    def update_salary(self):
+        self.salary += int(0.2*self.salary)
+
+employee = Employee(name='Tom', salary=2000)
+
+#### UI layer ####
+
+body_html = """
+    <div>
+        Employee name is {{employee.name}} <br>
+        Salary is ${{employee.salary}} <br>
+
+        <button ng-click='employee.update_salary()'>
+            Update salary
+        </button>
+    </div>
+"""
+template = Template(body_html=body_html)
+
+#### Entry point ####
+
+def main():
+    app = QtApp(template=template, context={'employee': employee})
+    app.start()
+
+if __name__ == '__main__':
+    main()
